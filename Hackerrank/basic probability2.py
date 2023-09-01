7ee0bd556725c13978172f8f095b347a75cf2ba2
chandrika16-sys <128931161+chandrika16-sys@users.noreply.github.com>
2023-09-01 03:03:18 -0700

    Update basic probability2.py

--- Hackerrank/basic probability2.py
+++ Hackerrank/basic probability2.py
@@ -1,1 +1,16 @@
+from fractions import Fraction
+production_a = 500 * 2
+pA = 0.005
+production_b = 1000 * 2
+pB = 0.008
+production_c = 2000 * 2
+pC = 0.010
+defA = ['defA' for _ in range(int(production_a * pA))]
+defB = ['defB' for _ in range(int(production_b * pB))]
+defC = ['defC' for _ in range(int(production_c * pC))]
 
+totalDef = defA + defB + defC
+
+probability = Fraction(len(defA), len(totalDef))
+
+print(probability)
